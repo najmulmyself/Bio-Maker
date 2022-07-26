dad72f0f27ae892ff3862cfb7aee9278e9630dfb
najmulmyself <najmul.myself@gmail.com>
2022-07-26 17:12:46 +0600

    commented container with textfield

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,55 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+              border: Border.all(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            child: Center(
+              child: Icon(
+                Icons.arrow_back_ios_new,
+              ),
+            ),
+          ),
+          Text('Welcome back! Glad \n to see you,Again!'),
+          // Container(
+          //   margin: EdgeInsets.symmetric(horizontal: 20),
+          //   height: 50,
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.grey.shade400),
+          //     borderRadius: BorderRadius.all(
+          //       Radius.circular(8),
+          //     ),
+          //     color: Colors.white10,
+          //   ),
+          //   child: Padding(
+          //     padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          //     child: TextField(
+          //       decoration: InputDecoration(
+          //         // focusedBorder: OutlineInputBorder(
+          //         //   borderSide: const BorderSide(color: Colors.red, width: 0),
+          //         //   borderRadius: BorderRadius.circular(25.0),
+          //         // ),
+          //         border: InputBorder.none,
+          //       ),
+          //     ),
+          //   ),
+          // )
+        ],
+      ),
+    );
   }
 }
