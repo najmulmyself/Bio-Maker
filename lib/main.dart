4fa015cb91c10b32bfe870d9d7052c77bb582c77
najmulmyself <najmul.myself@gmail.com>
2022-07-30 12:40:43 +0600

    added firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:bio_maker/pages/registration_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false ,
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: HomePage(),
       routes: {
-        '/login':(context) => LoginScreen(),
-        '/registration':(context) => RegistrationScreen()
+        '/login': (context) => LoginScreen(),
+        '/registration': (context) => RegistrationScreen()
       },
     );
   }
