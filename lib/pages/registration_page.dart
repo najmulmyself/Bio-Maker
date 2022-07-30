6cdbdc6f64b4db16d94a57358b2e4a07cd2a936e
najmulmyself <najmul.myself@gmail.com>
2022-07-30 14:30:18 +0600

    single chlid scrollview bug

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -1,3 +1,4 @@
+import 'package:bio_maker/utils/scaffold_msg.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -21,9 +22,14 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       final userCredential = FirebaseAuth.instance
           .createUserWithEmailAndPassword(
               email: emailController.text, password: passwordController.text);
+      if (userCredential != null) {
+        final util = Util(text: 'Registration Successfully');
+        util.showSnack(context);
+      }
     } on FirebaseAuthException catch (e) {
-      if(e.code == 'email-already-in-use'){
-        
+      if (e.code == 'email-already-in-use') {
+        final util = Util(text: 'Email already in use');
+        util.showSnack(context);
       }
     } catch (e) {
       print(e);
@@ -38,200 +44,206 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   );
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Container(
-                margin: EdgeInsets.only(top: 20),
-                alignment: Alignment.topLeft,
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(8),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(top: 20),
+                      alignment: Alignment.topLeft,
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                        border: Border.all(
+                          width: 2,
+                          color: Colors.grey.shade400,
+                        ),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.arrow_back_ios_new,
+                        ),
+                      ),
+                    ),
                   ),
-                  border: Border.all(
-                    width: 2,
-                    color: Colors.grey.shade400,
+                  Text(
+                    'Welcome back! Glad \n to see you,Again!',
+                    style: TextStyle(fontSize: 34),
                   ),
-                ),
-                child: Center(
-                  child: Icon(
-                    Icons.arrow_back_ios_new,
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        // margin: EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                          showCursor: true,
+                          cursorColor: Colors.grey.shade400,
+                          cursorHeight: 5,
+                          decoration: InputDecoration(
+                            // labelText: 'dflsadfjlsadkj',
+                            contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                            hintText: 'Username',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        // margin: EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                          controller: emailController,
+                          showCursor: true,
+                          cursorColor: Colors.grey.shade400,
+                          cursorHeight: 5,
+                          decoration: InputDecoration(
+                            // labelText: 'dflsadfjlsadkj',
+                            contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                            hintText: 'Email',
+
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        // margin: EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                          controller: passwordController,
+                          showCursor: true,
+                          cursorColor: Colors.grey.shade400,
+                          cursorHeight: 5,
+                          decoration: InputDecoration(
+                            // labelText: 'dflsadfjlsadkj',
+                            contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                            hintText: 'Password',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        // margin: EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                          showCursor: true,
+                          cursorColor: Colors.grey.shade400,
+                          cursorHeight: 5,
+                          decoration: InputDecoration(
+                            // labelText: 'dflsadfjlsadkj',
+                            contentPadding: EdgeInsets.symmetric(horizontal: 8),
+                            hintText: 'Confirm Password',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      // Container(
+                      //   margin: EdgeInsets.only(top: 10),
+                      //   alignment: AlignmentDirectional.centerEnd,
+                      //   child: Text(
+                      //     'Forget Password',
+                      //     style: TextStyle(
+                      //       fontSize: 16,
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
                   ),
-                ),
+                  bButton,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Divider(
+                        color: Colors.red,
+                        height: 5,
+                        thickness: 20,
+                      ),
+                      Text('Or login with'),
+                      Divider(
+                        height: 5,
+                        color: Colors.black,
+                        thickness: 2,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SocialButton(
+                        icon: FontAwesomeIcons.facebookF,
+                        // color: Colors.blue.shade700,
+                      ),
+                      SocialButton(
+                        icon: FontAwesomeIcons.apple,
+                        // color: Colors.green,
+                      ),
+                      SocialButton(
+                        icon: FontAwesomeIcons.google,
+                        // color: Colors.red,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Text('Already have an account ?'),
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                          child: Text('Login now')),
+                    ],
+                  )
+                ],
               ),
             ),
-            Text(
-              'Welcome back! Glad \n to see you,Again!',
-              style: TextStyle(fontSize: 34),
-            ),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(6),
-                    ),
-                  ),
-                  // margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: TextFormField(
-                    showCursor: true,
-                    cursorColor: Colors.grey.shade400,
-                    cursorHeight: 5,
-                    decoration: InputDecoration(
-                      // labelText: 'dflsadfjlsadkj',
-                      contentPadding: EdgeInsets.symmetric(horizontal: 8),
-                      hintText: 'Username',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(6),
-                    ),
-                  ),
-                  // margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: TextFormField(
-                    controller: emailController,
-                    showCursor: true,
-                    cursorColor: Colors.grey.shade400,
-                    cursorHeight: 5,
-                    decoration: InputDecoration(
-                      // labelText: 'dflsadfjlsadkj',
-                      contentPadding: EdgeInsets.symmetric(horizontal: 8),
-                      hintText: 'Email',
-
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(6),
-                    ),
-                  ),
-                  // margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: TextFormField(
-                    controller: passwordController,
-                    showCursor: true,
-                    cursorColor: Colors.grey.shade400,
-                    cursorHeight: 5,
-                    decoration: InputDecoration(
-                      // labelText: 'dflsadfjlsadkj',
-                      contentPadding: EdgeInsets.symmetric(horizontal: 8),
-                      hintText: 'Password',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(6),
-                    ),
-                  ),
-                  // margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: TextFormField(
-                    showCursor: true,
-                    cursorColor: Colors.grey.shade400,
-                    cursorHeight: 5,
-                    decoration: InputDecoration(
-                      // labelText: 'dflsadfjlsadkj',
-                      contentPadding: EdgeInsets.symmetric(horizontal: 8),
-                      hintText: 'Confirm Password',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                // Container(
-                //   margin: EdgeInsets.only(top: 10),
-                //   alignment: AlignmentDirectional.centerEnd,
-                //   child: Text(
-                //     'Forget Password',
-                //     style: TextStyle(
-                //       fontSize: 16,
-                //     ),
-                //   ),
-                // ),
-              ],
-            ),
-            bButton,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                Divider(
-                  color: Colors.red,
-                  height: 5,
-                  thickness: 20,
-                ),
-                Text('Or login with'),
-                Divider(
-                  height: 5,
-                  color: Colors.black,
-                  thickness: 2,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SocialButton(
-                  icon: FontAwesomeIcons.facebookF,
-                  // color: Colors.blue.shade700,
-                ),
-                SocialButton(
-                  icon: FontAwesomeIcons.apple,
-                  // color: Colors.green,
-                ),
-                SocialButton(
-                  icon: FontAwesomeIcons.google,
-                  // color: Colors.red,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                Text('Already have an account ?'),
-                GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    child: Text('Login now')),
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
--- lib/utils/scaffold_msg.dart
+++ lib/utils/scaffold_msg.dart
@@ -1,3 +1,13 @@
-class Util{
-  
+import 'package:flutter/material.dart';
+
+class Util {
+  final String? text;
+  Util({this.text});
+  void showSnack(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(text!),
+      ),
+    );
+  }
 }
