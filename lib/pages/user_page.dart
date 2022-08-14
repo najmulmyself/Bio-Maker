19b9f00e6e0356a152a77af45cf86d77aa5930ba
najmulmyself <najmul.myself@gmail.com>
2022-08-14 15:16:32 +0600

    added streambuilder need to read data

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,19 +1,41 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
   const UserPage({Key? key}) : super(key: key);
 
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  Stream userStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Users'),
       ),
-      body: Center(
-        child: Text('Users list will be shown here....'),
+      // body: Center(
+      //   child: Text('Users list will be shown here....'),
+      // ),
+      body: StreamBuilder(
+        stream: userStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('something went wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text('Data loading');
+          }
+          return ListView(
+            children: [Text('data')],
+          );
+        },
       ),
     );
   }
